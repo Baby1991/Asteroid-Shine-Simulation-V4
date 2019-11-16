f47c4eb0733ff0f0c570e472b294f3db6a27acb5
Filip Stevanovic <41167212+Baby1991@users.noreply.github.com>
2019-11-16 14:14:58 +0100

    Sim

--- Simulacija 4/Simulation.py
+++ Simulacija 4/Simulation.py
@@ -1,17 +1,19 @@
 from Asteroid_Geometry.Geometry import *
 
 from math import pi,cos,sin
-import numpy
 
-ref=Point(5,0)
-occluder=Line(Point(0,5),Point(0,3))
-linija=Line(Point(-2,2),Point(-2,-2))
+ast=Asteroid()
+"""ast.Circle(increment=pi/256,p=-0.9,q=-0.9)
+ast.Circle(increment=pi/256,p=0.9,q=-0.9)
+ast.Circle(increment=pi/256,p=-0.9,q=0.9)
+ast.Circle(increment=pi/256,p=0.9,q=0.9)
+ast.Test_Shine(phase=0,increment=pi/512)"""
+
+ast.Load("Asteroid")
+#ast.Test_Shine()
+#ast.Save()
 
 plot=Graph()
-plot.Line(linija,color="red",marker="o")
-visible=linija.Visibility(occluder,ref)
-if visible:
-    plot.Lines(visible,color="blue")
-plot.Line(occluder)
-plot.Point(ref)
+plot.Values(ast.shine)
+plot.Save("Shine")
 Graph.Show()
