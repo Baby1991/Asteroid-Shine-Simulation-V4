69a360830cb19ce528d8830ee7e85579d8d246b7
Baby1991 <41167212+Baby1991@users.noreply.github.com>
2019-11-18 00:01:41 +0100

    Fixing visibility detection

--- OldSim/Simulation.py
+++ OldSim/Simulation.py
@@ -3,23 +3,25 @@ from Geometry import *
 from math import pi,cos,sin
 import numpy as np
 
-ast=Asteroid("Asteroid 1",increment=pi/16,phase=0)
+ast=Asteroid("Asteroid 1",increment=pi,phase=0,radius=5)
 
-ast.Ellipse(increment=pi/16,p=-0.9,q=-0.9)
-ast.Ellipse(increment=pi/16,p=0.9,q=-0.9)
-ast.Ellipse(increment=pi/16,p=-0.9,q=0.9)
-ast.Ellipse(increment=pi/16 ,p=0.9,q=0.9)
+ast.Ellipse(increment=pi/16,p=-0.7)
+ast.Ellipse(increment=pi/16,p=0.7)
+ast.Ellipse(increment=pi/16,q=-0.7)
+ast.Ellipse(increment=pi/16,q=0.7)
 
 #ast.Load("Asteroid 512 512 1024")
-
+#ast.Plot().Save(ast.name)
 ast.Test()
 ast.Save()
 #Shutdown()
+plot=Graph()
+x,y,z=ast.visible[0]
+i=0
+plot.Lines(x,linewidth=5)
+for line in x:
 
-plot=Graph()
-r=[len(x) for x,y,z in ast.visible]
-plot.Values(r)
-plot=Graph()
-plot.Values(Filter(r))
-Graph.Show()
-    
+    plot.Line(line,color="red")
+    plot.Save(str(i))
+    i+=1
+
