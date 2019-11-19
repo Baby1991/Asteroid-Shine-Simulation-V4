82a6bccb4acda120e3fbe4f17ddd8e2c17d140cc
Baby1991 <41167212+Baby1991@users.noreply.github.com>
2019-11-19 18:36:28 +0100

    Cardiotida

--- OldSim/Simulation.py
+++ OldSim/Simulation.py
@@ -3,7 +3,7 @@ from Geometry import *
 from math import pi,cos,sin
 import numpy as np
 
-ast=Asteroid("Asteroid 1",increment=pi/32,phase=0)
+ast=Asteroid("Cardiotida",increment=pi/32,phase=0)
 
 text=LoadTxt("cardiotida.txt")
 points=[]
@@ -33,7 +33,7 @@ ast.Test(1)
 #print(len(ast.fixedLines))
 
 
-#ast.Save()
+ast.Save()
 
 """plot=Graph()
 plot.Lines(ast.lines,linewidth=5)
@@ -41,9 +41,7 @@ plot.Lines(ast.fixedLines,color="red")
 Graph.Show()"""
 #ast.Plot().Save(ast.name)
 #ast.shine.append(1)
-plot=Graph()
-plot.Values(ast.shine)
-Graph.Show()
+
 
 """for x,y,z in ast.visible:
     print(len(x))"""
