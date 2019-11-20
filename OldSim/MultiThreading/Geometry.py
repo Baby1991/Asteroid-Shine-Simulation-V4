bdb08c2bb9373b3a98d3e75fe5cd50b64b55249c
Baby1991 <41167212+Baby1991@users.noreply.github.com>
2019-11-20 12:59:49 +0100

    mlt fix3

--- OldSim/MultiThreading/Geometry.py
+++ OldSim/MultiThreading/Geometry.py
@@ -910,7 +910,7 @@ def print_progress_bar(iteration, total, prefix='', suffix='', decimals=1, lengt
                                                      (iteration / float(total)))
     filledLength = int(length * iteration // total)
     bar = fill * filledLength + '-' * (length - filledLength)
-    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end='\r')
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix)+'\r')
     if iteration == total:
         print('\n'+message)
 
